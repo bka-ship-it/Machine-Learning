9e32bc692fd880ec0532eb3e9e42f1f3ce1253ee
bka-ship-it <bka4120@gmail.com>
2026-03-14 17:13:14 +0000

    Add files via upload

--- Restaurant Reccommender System.py
+++ Restaurant Reccommender System.py
@@ -1,27 +1,30 @@
 import pandas as pd
-restaurant=pd.read_csv('washington_dc.csv')
+restaurant=pd.read_csv('washington_dc.txt', sep='\t')
+restaurant.to_csv('washington_dc.csv',index=False)
 print(restaurant.info())
 print(restaurant.head())
 
-ratings=pd.read_csv('features.csv')
-print(ratings.info())
-print(ratings.head())
+features=pd.read_csv('features.txt', sep='\t')
+features.to_csv('features.csv',index=False)
+print(features.info())
+print(features.head())
 
-rest_data=pd.read_csv('new_york.csv')
+rest_data=pd.read_csv('new_york.txt', sep='\t')
+rest_data.to_csv('new_york.csv',index=False)
 print(rest_data.head())
 
-C=rest_data['vote_average'].mean()
-m=rest_data['vote_count'].quantile(0.90)
+C=features['feature_average'].mean()
+m=features['feature_count'].quantile(0.90)
 
-q_rest=rest_data.copy().loc[rest_data['vote_count']>=m]
+q_rest=features.copy().loc[features['feature_count']>=m]
 print(q_rest.shape)
 
 def weighted_rating(x,m=m,C=C):
-    v=x['vote_count']
-    R=x['vote_average']
+    v=x['feature_count']
+    R=x['feature_average']
     return (v/(v+m)*R) + (m/(m+v)*C)
 
 q_rest['score']=q_rest.apply(weighted_rating, axis=1)
 q_rest=q_rest.sort_values('score',ascending=False)
 
-print(q_rest[['title','vote_count','vote_average','score']].head(20))
+print(q_rest[['title','feature_count','feature_average','score']].head(20))
